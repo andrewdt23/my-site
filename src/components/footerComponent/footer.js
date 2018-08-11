c61ce7607f40655e2a94ad162aacf32c41e50575
Andrew Thompson <Andrew@Andrews-MacBook-Pro-3.local>
2018-08-11 09:28:38 -0700

    adjust footer for mobile and full

--- src/components/footerComponent/footer.js
+++ src/components/footerComponent/footer.js
@@ -2,24 +2,55 @@ import React, {Component} from 'react';
 import {Navbar, Nav, NavItem} from 'react-bootstrap'
 
 class Footer extends Component {
+
+    renderFullFooter() {
+        return (
+            <div className="full-footer">
+                <Navbar className="footer" fluid="fluid" fixedBottom="fixedBottom">
+                    <Navbar.Header>
+                        <Navbar.Collapse>
+                            <Navbar.Brand>
+                                <i class="far fa-copyright"></i>
+                                2018 Andrew Thompson
+                            </Navbar.Brand>
+                        </Navbar.Collapse>
+                    </Navbar.Header>
+                    <Nav className="footer-nav" pullRight="pullRight">
+                        <NavItem className="github" href="https://github.com/andrewdt23/my-site" target="__blank">
+                            Check out the Github repo for this site
+                        </NavItem>
+                    </Nav>
+                </Navbar>
+            </div>
+        );
+    }
+
+    renderMobileFooter() {
+        return (
+            <div className="mobile-footer">
+                <Navbar className="footer" fluid="fluid" staticBottom="staticBottom">
+                    <Navbar.Header>
+                        <Navbar.Collapse>
+                            <Navbar.Brand>
+                                <i class="far fa-copyright"></i>
+                                2018 Andrew Thompson
+                            </Navbar.Brand>
+                        </Navbar.Collapse>
+                    </Navbar.Header>
+                    <Nav className="footer-nav" pullRight="pullRight">
+                        <NavItem className="github" href="https://github.com/andrewdt23/my-site" target="__blank">
+                            Check out the Github repo for this site
+                        </NavItem>
+                    </Nav>
+                </Navbar>
+            </div>
+        );
+    }
+
     render() {
         return (<footer className="footer-wrapper">
-
-            <Navbar className="footer" fluid="fluid" fixedBottom="fixedBottom">
-                <Navbar.Header>
-                    <Navbar.Collapse>
-                        <Navbar.Brand>
-                            <i class="far fa-copyright"></i>
-                            2018 Andrew Thompson
-                        </Navbar.Brand>
-                    </Navbar.Collapse>
-                </Navbar.Header>
-                <Nav className="footer-nav" pullRight="pullRight">
-                    <NavItem className="github" href="https://github.com/andrewdt23/my-site" target="__blank">
-                        Check out the Github repo for this site
-                    </NavItem>
-                </Nav>
-            </Navbar>
+            {this.renderFullFooter()}
+            {this.renderMobileFooter()}
         </footer>);
     }
 }
