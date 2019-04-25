a17b6dc1f9f5a61bdab0e36225cbacc796302392
Andrew Thompson <andrewdt23@Andrews-MacBook-Pro.local>
2019-04-25 12:26:10 -0700

    Added Tidal to projects page

--- src/components/pages/projects.js
+++ src/components/pages/projects.js
@@ -7,9 +7,16 @@ class Projects extends Component {
         <h1 className = 'project-title'>
         My Projects
         </h1>
-        <h2 className = 'project-body'>
-            Coming Soon!
-        </h2>
+        <div className = 'project-body'>
+            <div className='tidal-photo'>
+                <a href="https://tidal.netlify.com/" target="_blank" rel="noopener noreferrer">
+                    <img src="https://uniim1.shutterfly.com/ng/services/mediarender/THISLIFE/022023533722/media/114516057087/large/1556218797/enhance" alt="Tidal"/>
+                </a>
+                <div className='tidal-body'>
+                    <p className='tidal-description'>Tidal is a single-page web app which targets a user's location to display county-based tide information. I created this application because I wanted a quick and clean way to check the tide for surfing.</p>
+                </div>
+            </div>
+        </div>
       </div>
     );
   }
